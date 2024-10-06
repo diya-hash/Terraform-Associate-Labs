502315bc59213ee84e80e0df9fc39197aa42dd4a
Diya <shaikh.diya@northsouth.edu>
2024-10-06 01:28:14 -0400

    updated local exec and remote exec

--- 010_provisioners-cloud-init/main.tf
+++ 010_provisioners-cloud-init/main.tf
@@ -1,14 +1,14 @@
-terraform { 
-#   backend "remote" { 
-#     organization = "org_diya" 
+terraform {
+  #   backend "remote" { 
+  #     organization = "org_diya" 
 
-#     workspaces { 
-#       name = "provisioners" 
-#     } 
-#   } 
+  #     workspaces { 
+  #       name = "provisioners" 
+  #     } 
+  #   } 
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.69.0"
     }
   }
@@ -27,42 +27,42 @@ resource "aws_security_group" "sg_my_server" {
 
   ingress = [
     {
-    description = "HTTP"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = [] 
-    prefix_list_ids = []
-    security_groups = []
-    self = false
-  },
-  {
-    description = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["99.240.67.76/32"]
-    ipv6_cidr_blocks = [] 
-    prefix_list_ids = []
-    security_groups = []
-    self = false
-  }
-  ] 
+      description      = "HTTP"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    },
+    {
+      description      = "SSH"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["99.240.67.76/32"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
 
   egress = [
     {
-    description = "outgoing traffic"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-    prefix_list_ids = []
-    security_groups = []
-    self = false
-  }
-  ] 
+      description      = "outgoing traffic"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
 
 }
 resource "aws_key_pair" "deployer" {
@@ -70,37 +70,47 @@ resource "aws_key_pair" "deployer" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCkNvI+H3XQUQAxor2xxx0QY2jwJwhPhP1mD+ETduDy/u58jkO9PZdr6uD9/7akNi8Hi48hwl9w9EACO5vgfD4H4OQ2KMOl8/0JCyhMQSiGISSc+j3Af9xfzgIP4qbRNSLpcIRLLEBFAJuE2vXpkhM4MCSF+jeksNXRLdfm3HyMVQiGnxIR1o/gEutLqdiQWxTMaxA0BICmU4IoyrmLN+jfpPdDq1GPvBWCoDVbDxYFjxabMq31g6SEBlZXsj2orpffJVgBA4zSqlzaUlttsE/oXHb7QZQ54omQAW+xMyecThN7LpyG/f/gLV95gHg2J02CGl10QdUDHQYnDMb522kwUiiu1iMvdnngssMmLMF+vbkDNmkRnquSh7f0qWyE87or9lqwx9KIbjCyYWJYoD4TjzpTCp+PAAlCCa0MiorBX2N5LOS7nB9iLtyZPo8+Fu6E/XBhtalb9t+4dV3BhcVaYuL9aGMWECQBHQMEmDQG9y9gpMJFqJxsj5ANyrTW+UE= shaikhdiyasalam@Shaikhs-MacBook-Air.local"
 }
 
-data "template_file" "user_data"{
-    template = file("./userdata.yaml")
+data "template_file" "user_data" {
+  template = file("./userdata.yaml")
 }
 resource "aws_instance" "my_server" {
   ami           = "ami-087c17d1fe0178315"
   instance_type = "t2.micro"
-  user_data = data.template_file.user_data.rendered
-#   provisioner "local-exec" {
-#     command = "echo ${self.private_ip} >> private_ips.txt"
-#   } done in the local-exec class
-  provisioner "remote-exec" {
-    inline = [
-    #   "echo /home/ec2-user",
-    #   "puppet apply",
-    #   "consul join ${aws_instance.web.private_ip}",
-      "echo ${self.private_ip} >> /home/ec2-user/private_ips.txt"
-    ]
+  user_data     = data.template_file.user_data.rendered
+  #   provisioner "local-exec" {
+  #     command = "echo ${self.private_ip} >> private_ips.txt"
+  #   } done in the local-exec class
+  #   provisioner "remote-exec" {
+  #     inline = [
+  #     #   "echo /home/ec2-user",
+  #     #   "puppet apply",
+  #     #   "consul join ${aws_instance.web.private_ip}",
+  #       "echo ${self.private_ip} >> /home/ec2-user/private_ips.txt"
+  #     ]
+  #     connection {
+  #     type     = "ssh"
+  #     user     = "ec2-user"
+  #     host     = "${self.public_ip}"
+  #     private_key = "${file("/Users/shaikhdiyasalam/.ssh/terraform")}"
+  #   }
+  #   } # remote-exec class
+  provisioner "file" {
+    content     = "mars"
+    destination = "/home/ec2-user/barsoon.txt"
     connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    host     = "${self.public_ip}"
-    private_key = "${file("/Users/shaikhdiyasalam/.ssh/terraform")}"
+      type        = "ssh"
+      user        = "ec2-user"
+      host        = self.public_ip
+      private_key = file("/Users/shaikhdiyasalam/.ssh/terraform")
+    }
   }
-  } # remote-exec class
-  key_name = "${aws_key_pair.deployer.key_name}"
+  key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.sg_my_server.id]
   tags = {
     Name = "MyServer"
   }
 }
-output "public_ip"{
-    value = aws_instance.my_server.public_ip
+output "public_ip" {
+  value = aws_instance.my_server.public_ip
 }
 
--- 011_provisioners-local-exec/main.tf
+++ 011_provisioners-local-exec/main.tf
@@ -1,8 +1,15 @@
 terraform {
+  #   backend "remote" { 
+  #     organization = "org_diya" 
+
+  #     workspaces { 
+  #       name = "provisioners" 
+  #     } 
+  #   } 
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "3.59.0"
+      source  = "hashicorp/aws"
+      version = "5.69.0"
     }
   }
 }
@@ -10,15 +17,12 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
 data "aws_vpc" "main" {
-  id = "vpc-bd9bdcc7"
+  id = "vpc-06771758657a140ad"
 }
-
-
 resource "aws_security_group" "sg_my_server" {
   name        = "sg_my_server"
-  description = "MyServer Security Group"
+  description = "My Server Security Group"
   vpc_id      = data.aws_vpc.main.id
 
   ingress = [
@@ -29,63 +33,70 @@ resource "aws_security_group" "sg_my_server" {
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = []
-			prefix_list_ids  = []
-			security_groups = []
-			self = false
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
     },
     {
       description      = "SSH"
       from_port        = 22
       to_port          = 22
       protocol         = "tcp"
-      cidr_blocks      = ["104.194.51.113/32"]
+      cidr_blocks      = ["99.240.67.76/32"]
       ipv6_cidr_blocks = []
-			prefix_list_ids  = []
-			security_groups = []
-			self = false
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
     }
   ]
 
   egress = [
     {
-			description = "outgoing traffic"
+      description      = "outgoing traffic"
       from_port        = 0
       to_port          = 0
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
-			prefix_list_ids  = []
-			security_groups = []
-			self = false
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
     }
   ]
-}
 
+}
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
-  public_key = "YOUR_SSH_KEY"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCkNvI+H3XQUQAxor2xxx0QY2jwJwhPhP1mD+ETduDy/u58jkO9PZdr6uD9/7akNi8Hi48hwl9w9EACO5vgfD4H4OQ2KMOl8/0JCyhMQSiGISSc+j3Af9xfzgIP4qbRNSLpcIRLLEBFAJuE2vXpkhM4MCSF+jeksNXRLdfm3HyMVQiGnxIR1o/gEutLqdiQWxTMaxA0BICmU4IoyrmLN+jfpPdDq1GPvBWCoDVbDxYFjxabMq31g6SEBlZXsj2orpffJVgBA4zSqlzaUlttsE/oXHb7QZQ54omQAW+xMyecThN7LpyG/f/gLV95gHg2J02CGl10QdUDHQYnDMb522kwUiiu1iMvdnngssMmLMF+vbkDNmkRnquSh7f0qWyE87or9lqwx9KIbjCyYWJYoD4TjzpTCp+PAAlCCa0MiorBX2N5LOS7nB9iLtyZPo8+Fu6E/XBhtalb9t+4dV3BhcVaYuL9aGMWECQBHQMEmDQG9y9gpMJFqJxsj5ANyrTW+UE= shaikhdiyasalam@Shaikhs-MacBook-Air.local"
 }
 
 data "template_file" "user_data" {
-	template = file("./userdata.yaml")
+  template = file("./userdata.yaml")
 }
-
-
 resource "aws_instance" "my_server" {
   ami           = "ami-087c17d1fe0178315"
   instance_type = "t2.micro"
-	key_name = "${aws_key_pair.deployer.key_name}"
-	vpc_security_group_ids = [aws_security_group.sg_my_server.id]
-	user_data = data.template_file.user_data.rendered
+  user_data     = data.template_file.user_data.rendered
   provisioner "local-exec" {
     command = "echo ${self.private_ip} >> private_ips.txt"
   }
-
+  provisioner "file" {
+    content     = "mars"
+    destination = "/home/ec2-user/barsoon.txt"
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      host        = self.public_ip
+      private_key = file("/Users/shaikhdiyasalam/.ssh/terraform")
+    }
+  }
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.sg_my_server.id]
   tags = {
     Name = "MyServer"
   }
 }
-
-output "public_ip"{
-	value = aws_instance.my_server.public_ip
+output "public_ip" {
+  value = aws_instance.my_server.public_ip
 }
+
