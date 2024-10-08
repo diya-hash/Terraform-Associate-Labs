bb040209b3ee8453198383a47f01288e8569fe79
Diya <shaikh.diya@northsouth.edu>
2024-10-08 13:03:08 -0400

    committing null-resource

--- 013_provisioners-file/main.tf
+++ 013_provisioners-file/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "3.59.0"
     }
   }
@@ -29,9 +29,9 @@ resource "aws_security_group" "sg_my_server" {
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = []
-			prefix_list_ids  = []
-			security_groups = []
-			self = false
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
     },
     {
       description      = "SSH"
@@ -40,23 +40,23 @@ resource "aws_security_group" "sg_my_server" {
       protocol         = "tcp"
       cidr_blocks      = ["104.194.51.113/32"]
       ipv6_cidr_blocks = []
-			prefix_list_ids  = []
-			security_groups = []
-			self = false
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
     }
   ]
 
   egress = [
     {
-			description = "outgoing traffic"
+      description      = "outgoing traffic"
       from_port        = 0
       to_port          = 0
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
-			prefix_list_ids  = []
-			security_groups = []
-			self = false
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
     }
   ]
 }
@@ -67,25 +67,25 @@ resource "aws_key_pair" "deployer" {
 }
 
 data "template_file" "user_data" {
-	template = file("./userdata.yaml")
+  template = file("./userdata.yaml")
 }
 
 
 resource "aws_instance" "my_server" {
-  ami           = "ami-087c17d1fe0178315"
-  instance_type = "t2.micro"
-	key_name = "${aws_key_pair.deployer.key_name}"
-	vpc_security_group_ids = [aws_security_group.sg_my_server.id]
-	user_data = data.template_file.user_data.rendered
+  ami                    = "ami-087c17d1fe0178315"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.sg_my_server.id]
+  user_data              = data.template_file.user_data.rendered
   provisioner "file" {
     content     = "mars"
     destination = "/home/ec2-user/barsoon.txt"
-		connection {
-			type     = "ssh"
-			user     = "ec2-user"
-			host     = "${self.public_ip}"
-			private_key = "${file("/root/.ssh/terraform")}"
-		}
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      host        = self.public_ip
+      private_key = file("/root/.ssh/terraform")
+    }
   }
 
   tags = {
@@ -93,6 +93,6 @@ resource "aws_instance" "my_server" {
   }
 }
 
-output "public_ip"{
-	value = aws_instance.my_server.public_ip
+output "public_ip" {
+  value = aws_instance.my_server.public_ip
 }
