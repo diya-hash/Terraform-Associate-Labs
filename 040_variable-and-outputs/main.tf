6e5b8687aced5b7f9d1bf60ffb8bdb9e712a5eaa
Diya <shaikh.diya@northsouth.edu>
2024-10-11 10:49:49 -0400

    variable input and output done

--- 040_variable-and-outputs/main.tf
+++ 040_variable-and-outputs/main.tf
@@ -1,11 +1,33 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
-module "aws_server" {
-	source = ".//aws_server"
-	instance_type = "t2.micro"
+variable "instance_type" {
+  type        = string
+  description = "The size of the instance."
+  #   sensitive   = true
+  validation {
+    condition     = can(regex("^t2.", var.instance_type))
+    error_message = "the instance must be a t2 type EC2 instance"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my_server" {
+  ami           = "ami-087c17d1fe0178315"
+  instance_type = var.instance_type
 }
 
 output "public_ip" {
-  value = module.aws_server.public_ip
+  value       = aws_instance.my_server.public_ip
+  sensitive   = true
+  description = "The public ip of the instance"
 }
