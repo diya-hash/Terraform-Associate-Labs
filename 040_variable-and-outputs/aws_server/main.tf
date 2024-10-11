8fb71f3fe04fac0d214aad6b9d8b05de4dee3360
Diya <shaikh.diya@northsouth.edu>
2024-10-11 12:44:32 -0400

    local variables completed

---
+++ 040_variable-and-outputs/aws_server/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+variable "instance_type" {
+  type        = string
+  description = "The size of the instance."
+  #   sensitive   = true
+  validation {
+    condition     = can(regex("^t2.", var.instance_type))
+    error_message = "the instance must be a t2 type EC2 instance"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+locals {
+  ami           = "ami-087c17d1fe0178315"
+  instance_type = var.instance_type
+}
+
+resource "aws_instance" "my_server" {
+  ami           = local.ami
+  instance_type = local.instance_type
+
+}
+
+output "public_ip" {
+  value     = aws_instance.my_server.public_ip
+  sensitive = false
+}
+
