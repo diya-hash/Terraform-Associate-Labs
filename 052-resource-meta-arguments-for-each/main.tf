7eebd12f6ea574b5eb68b5a6da14823c4cffd6f1
Diya <shaikh.diya@northsouth.edu>
2024-10-15 13:09:26 -0400

    completed alias and meta args

--- 052-resource-meta-arguments-for-each/main.tf
+++ 052-resource-meta-arguments-for-each/main.tf
@@ -2,28 +2,30 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.58.0"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
+  region = "us-east-1"
 }
+
 resource "aws_instance" "my_server" {
-	for_each = {
-		nano = "t2.nano"
-		micro =  "t2.micro"
-		small =  "t2.small"
-	}
-  ami           = "ami-087c17d1fe0178315"
+  for_each = {
+    nano  = "t2.nano"
+    micro = "t2.micro"
+    small = "t2.small"
+  }
+  ami           = "ami-0fff1b9a61dec8a5f"
   instance_type = each.value
-	tags = {
-		Name = "Server-${each.key}"
-	}
+  tags = {
+    Name = "Server-${each.key}"
+  }
 }
 
 output "public_ip" {
-  value = values(aws_instance.my_server)[*].public_ip
+  value     = values(aws_instance.my_server)[*].public_ip
+  sensitive = false
 }
+
